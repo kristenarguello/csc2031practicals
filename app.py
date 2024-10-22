b41af9135ada413ebcaa950dddc75df631a38df1
Kristen <kristenarguello20@gmail.com>
2024-10-22 12:28:01 +0100

    change template file name for rate limit error

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 
 @app.errorhandler(429)
 def rate_limit(e):
-    return render_template("errors/requests_limit.html"), 429
+    return render_template("errors/rate_limit.html"), 429
 
 
 if __name__ == "__main__":
