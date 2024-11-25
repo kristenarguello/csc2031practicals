f23b3a468a745085859a351b4b89a661f7c07451
Kristen <kristenarguello20@gmail.com>
2024-11-25 16:13:12 +0000

    practical 18 - input validation with regex

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,45 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length, Regexp
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp
+
+# only valid if contain letters or hyphens
+NAME_REGEX = "^[a-zA-Z-]+$"
+
+# options for uk landline numbers
+THREE_CODE_DIGITS = "(02[0-9]{1}-[0-9]{8})"
+FOUR_CODE_DIGITS = "((011[0-9]{1}-[0-9]{7})|(01[0-9]{1}1-[0-9]{7}))"
+FIVE_CODE_DIGITS = "((01[0-9]{3}-[0-9]{5})|(01[0-9]{3}-[0-9]{6}))"
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField(validators=[DataRequired()])
-    firstname = StringField(validators=[DataRequired()])
-    lastname = StringField(validators=[DataRequired()])
-    phone = StringField(validators=[DataRequired()])
+    email = StringField(validators=[DataRequired(), Email()])
+    firstname = StringField(
+        validators=[
+            DataRequired(),
+            Regexp(
+                regex=NAME_REGEX,
+                message="First name must follow pattern of letters and/or hyphen.",
+            ),
+        ]
+    )
+    lastname = StringField(
+        validators=[
+            DataRequired(),
+            Regexp(
+                regex=NAME_REGEX,
+                message="Last name must follow pattern of letters and/or hyphen.",
+            ),
+        ]
+    )
+    phone = StringField(
+        validators=[
+            DataRequired(),
+            Regexp(
+                regex=f"^({THREE_CODE_DIGITS}|{FOUR_CODE_DIGITS}|{FIVE_CODE_DIGITS})$",
+                message="Phone number must be a valid UK landline number.",
+            ),
+        ]
+    )
     password = PasswordField(
         validators=[
             DataRequired(),
